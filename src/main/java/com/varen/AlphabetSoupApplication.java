d1ef15c1d9e3b462b103c6d4baeb209e7dc6357b
Philip Buonomo <philip.buonomo@am.jll.com>
2021-05-18 22:53:04 -0400

    use try-with-resources to ensure the Reader is closed

--- src/main/java/com/varen/AlphabetSoupApplication.java
+++ src/main/java/com/varen/AlphabetSoupApplication.java
@@ -12,44 +12,47 @@ import java.util.stream.IntStream;
 
 public class AlphabetSoupApplication {
 	private static String fileUrl = "C:\\Users\\buonomo\\Desktop\\alphabet-soup.input";
+	private static String fileCharset = "Cp1252";
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Begin Alphabet Soup:");
 
-		FileInputStream inputFileReader = new FileInputStream(new File(fileUrl));
-		BufferedReader bufferedFileReader = new BufferedReader(new InputStreamReader(inputFileReader, "Cp1252"));
+		try (BufferedReader bufferedFileReader = new BufferedReader(
+				new InputStreamReader(new FileInputStream(new File(fileUrl)), fileCharset))) {
 
-		List<String> inputLines = new ArrayList<String>();
-		HashMap< Integer, List<String> > hiddenWordsByWordLength = new HashMap< Integer, List<String> >();
-		
-		bufferedFileReader.lines().forEach((String line) -> {
-			inputLines.add(line);
-		});
+			List<String> inputLines = new ArrayList<String>();
 
-		String gridDimensions = inputLines.get(0);
-		Integer numberOfRows = Integer.valueOf(gridDimensions.charAt(0) + "");
-		Integer numberOfCols = Integer.valueOf(gridDimensions.charAt(2) + "");
-		
-		// Our set of hidden words begins at index (line number) equal to numberOfRows + 1
-		IntStream.range(numberOfRows + 1, inputLines.size()).forEach( (i -> {
-			String word = inputLines.get(i);
-			
-			if(hiddenWordsByWordLength.containsKey(word.length())) {
-				hiddenWordsByWordLength.get(word.length()).add(word);
-			}
-			else {
-				List<String> newListForLength = new ArrayList<String>();
-				newListForLength.add(word);
-				hiddenWordsByWordLength.put(word.length(), newListForLength);
-			}
-		}));
-		
-		// Our word scramble grid begins at index (line number) 1
-		IntStream.range(1, numberOfRows + 1).forEach( (i -> {
-			String row = inputLines.get(i);
-		}));
-		
-		hiddenWordsByWordLength.keySet().forEach( (i) -> System.out.println("Word size: "+i+" / Count = "+hiddenWordsByWordLength.get(i).size()));
+			// Map is keyed by length of words contained in the collection held by the value
+			HashMap<Integer, List<String>> hiddenWordsMap = new HashMap<Integer, List<String>>();
+
+			bufferedFileReader.lines().forEach((String line) -> {
+				inputLines.add(line);
+			});
+
+			String gridDimensions = inputLines.get(0);
+			Integer numberOfRows = Integer.valueOf(gridDimensions.charAt(0) + "");
+			Integer numberOfCols = Integer.valueOf(gridDimensions.charAt(2) + "");
+
+			// Our set of hidden words begins at index (line number) equal to numberOfRows +
+			// 1
+			IntStream.range(numberOfRows + 1, inputLines.size()).forEach((i -> {
+				String word = inputLines.get(i);
+
+				if (hiddenWordsMap.containsKey(word.length())) {
+					hiddenWordsMap.get(word.length()).add(word);
+				} else {
+					List<String> newListForLength = new ArrayList<String>();
+					newListForLength.add(word);
+					hiddenWordsMap.put(word.length(), newListForLength);
+				}
+			}));
+
+			// Our word scramble grid begins at index (line number) 1
+			IntStream.range(1, numberOfRows + 1).forEach((i -> {
+				String row = inputLines.get(i);
+			}));
+
+			hiddenWordsMap.keySet().forEach(
+					(i) -> System.out.println("Word size: " + i + " / Count = " + hiddenWordsMap.get(i).size()));
+		}
 	}
-
 }
